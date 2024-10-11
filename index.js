81d3c26de04a512e82a042b48cdef16bf1e67b9a
Kevin Jones <143275279+kevinvj0131@users.noreply.github.com>
2024-10-10 21:31:48 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-function turnOffBackground() {
-    let sideA = document.getElementById('sideA');
-    let sideB = document.getElementById('sideB');
-    let logo = document.getElementById('logo');
-    currentBackground = window.getComputedStyle(sideA).backgroundColor;
-    currentBackground2 = window.getComputedStyle(sideB).backgroundColor;
-    currentBackground3 = window.getComputedStyle(logo).backgroundColor;
-    if (currentBackground === 'rgba(0, 255, 255, 0)' &&
-        currentBackground2 === 'rgba(127, 255, 212, 0)' &&
-        currentBackground3 === 'rgba(255, 0, 0, 0)') {
-        sideA.style.backgroundColor = 'rgba(0, 255, 255, 0.5)';
-        sideB.style.backgroundColor = 'rgba(127, 255, 212, 0.5)';
-        logo.style.backgroundColor = 'rgba(255, 0, 0, 1)';
-    } else {
-        sideA.style.backgroundColor = 'rgba(0, 255, 255, 0)'
-        sideB.style.backgroundColor = 'rgba(127, 255, 212, 0)';
-        logo.style.backgroundColor = 'rgba(255, 0, 0, 0)';
-    }
-
-}
-
 function changeLogo() {
     let logo = document.getElementById('logo');
     let anchor = document.getElementById('anchor');
